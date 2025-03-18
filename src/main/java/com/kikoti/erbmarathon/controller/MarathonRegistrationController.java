589939141f992effafefbf9d075cc447fb43d362
amkikoti <tonykikoti@gmail.com>
2025-03-18 16:17:28 +0300

    Fix Marathon Registration

--- src/main/java/com/kikoti/erbmarathon/controller/MarathonRegistrationController.java
+++ src/main/java/com/kikoti/erbmarathon/controller/MarathonRegistrationController.java
@@ -3,31 +3,38 @@ package com.kikoti.erbmarathon.controller;
 import com.kikoti.erbmarathon.dtos.MarathonRegistrationDto;
 import com.kikoti.erbmarathon.entity.MarathonRegistration;
 import com.kikoti.erbmarathon.services.marathon.MarathonRegistrationService;
+import com.kikoti.erbmarathon.utils.ResponseWrapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/marathons")
+@RequestMapping("/api/marathon-registration")
 @RequiredArgsConstructor
 public class MarathonRegistrationController {
+    private final MarathonRegistrationService marathonRegistrationService;
 
-    private final MarathonRegistrationService registrationService;
-
-    @PostMapping("/register")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<MarathonRegistration> register(@RequestBody MarathonRegistrationDto registrationDto) {
-        return new ResponseEntity<>(
-                registrationService.registerForMarathon(registrationDto),
-                HttpStatus.CREATED
-        );
+    @PostMapping
+    public ResponseEntity<ResponseWrapper<MarathonRegistration>> createOrUpdateMarathon(@Valid @RequestBody MarathonRegistrationDto request) {
+        return marathonRegistrationService.saveMarathonRegistration(request);
     }
 
-    @GetMapping("/{marathonId}/my-registration")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<MarathonRegistration> getMyRegistration(@PathVariable Long marathonId) {
-        return ResponseEntity.ok(registrationService.getCurrentUserRegistration(marathonId));
+    @GetMapping
+    public ResponseEntity<ResponseWrapper<Page<MarathonRegistration>>> getAllMarathons(
+            @RequestParam(defaultValue = "0") int page,    // Default page number is 0
+            @RequestParam(defaultValue = "10") int size) {  // Default page size is 10
+        return marathonRegistrationService.getAllRegistrations(page, size);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseWrapper<MarathonRegistration>> getMarathonById(@PathVariable Long id) {
+        return marathonRegistrationService.getRegistrationById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseWrapper<Void>> deleteMarathon(@PathVariable Long id) {
+        return marathonRegistrationService.deleteRegistration(id);
     }
 }
